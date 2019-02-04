2520d1c6a8869b283941ccec79c102bf60ef08a3
crystal <crystal2840@gmail.com>
2019-02-04 17:36:19 +0900

    :fire: 테스트코드 리팩토링

--- src/test/java/me/crystal/demoinfleanrestapi/events/EventControllerTests.java
+++ src/test/java/me/crystal/demoinfleanrestapi/events/EventControllerTests.java
@@ -1,26 +1,15 @@
 package me.crystal.demoinfleanrestapi.events;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import me.crystal.demoinfleanrestapi.common.RestDocsConfiguration;
+import me.crystal.demoinfleanrestapi.common.BaseControllerTest;
 import me.crystal.demoinfleanrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.beans.MethodDescriptor;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
@@ -33,25 +22,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTests {
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
+public class EventControllerTests extends BaseControllerTest {
 
     @Autowired
     EventRepository eventRepository;
-
-    @Autowired
-    ModelMapper modelMapper;
 
 
     @Test
