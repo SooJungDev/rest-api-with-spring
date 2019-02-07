8a41524939272c128dd87ed431d530a84d91aec2
crystal <crystal2840@gmail.com>
2019-02-07 11:51:33 +0900

    :fire: 시큐리티 폼 인증 설정

--- src/main/java/me/crystal/demoinfleanrestapi/accounts/AccountService.java
+++ src/main/java/me/crystal/demoinfleanrestapi/accounts/AccountService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -20,16 +21,24 @@ public class AccountService implements UserDetailsService {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Account saveAccount(Account account) {
+        account.setPassword(this.passwordEncoder.encode(account.getPassword()));
+        return this.accountRepository.save(account);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(username)
-                .orElseThrow(()-> new UsernameNotFoundException(username));
+                .orElseThrow(() -> new UsernameNotFoundException(username));
         return new User(account.getEmail(), account.getPassword(), authorites(account.getRoles()));
     }
 
     private Collection<? extends GrantedAuthority> authorites(Set<AccountRole> roles) {
         return roles.stream().
-                map(r -> new SimpleGrantedAuthority("ROLE_"+r.name()))
+                map(r -> new SimpleGrantedAuthority("ROLE_" + r.name()))
                 .collect(Collectors.toSet());
     }
 }
--- src/main/java/me/crystal/demoinfleanrestapi/configs/AppConfig.java
+++ src/main/java/me/crystal/demoinfleanrestapi/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package me.crystal.demoinfleanrestapi.configs;
 
+import me.crystal.demoinfleanrestapi.accounts.Account;
+import me.crystal.demoinfleanrestapi.accounts.AccountRole;
+import me.crystal.demoinfleanrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -18,4 +26,24 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Bean
+    public ApplicationRunner applicationRunner(){
+        return  new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account soojung = Account.builder()
+                        .email("soojung@email.com")
+                        .password("soojung")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+
+                accountService.saveAccount(soojung);
+
+            }
+        };
+    }
+
 }
